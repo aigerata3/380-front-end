4d415465d62089b1dbd87452f71e6ac95bab7fc9
aigerata3 <77941309+aigerata3@users.noreply.github.com>
2023-03-27 17:40:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+
+}
+
+
+
+
+//let navbar = document.querySelector('.header .navbar');
+
+//document.querySelector('#menu-btn').onclick = () =>{
+  //  navbar.classList.toggle('active');
+//}
+
+//window.onscroll = () => {
+  //  navbar.classList.remove('active');
+//}
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,},
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
